ea48552579b6c2f072b32bfad0e11cf21dc1c50c
khantpyaehtoo <ericrebillet@gmail.com>
2026-06-29 15:49:34 +0630

    service create form

--- src/features/management/services/ServiceHeader.jsx
+++ src/features/management/services/ServiceHeader.jsx
@@ -1,18 +1,21 @@
 import {
     ArrowLeftOutlined,
+    PlusCircleOutlined,
     // PlusCircleOutlined,
     SearchOutlined,
 } from "@ant-design/icons";
-import { Button, Input, Space } from "antd";
+import { Button, Form, Input, Modal, Space } from "antd";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import AddForm from "../../../components/modals/AddForm";
 
 const ServiceHeader = ({ title }) => {
     const nav = useNavigate();
     const [searchText, setSearchText] = useState("");
-    const [isOpen, setIsopen] = useState(false);
+    const [isFormOpen, setIsFormopen] = useState(false);
     const [isEdit, setIsEdit] = useState(false);
+
+    const { form } = Form.useForm();
+    const { Item } = Form;
 
     return (
         <>
@@ -40,21 +43,120 @@ const ServiceHeader = ({ title }) => {
                         size="large"
                     />
 
-                    {/* <Button
+                    <Button
                         variant="solid"
                         icon={<PlusCircleOutlined />}
                         className="createFormBtn!"
+                        onClick={() => setIsFormopen(true)}
                     >
                         Create {title}
-                    </Button> */}
+                    </Button>
 
-                    <AddForm
-                        title={title}
-                        isEdit={isEdit}
-                        isOpen={isOpen}
-                        // triggerCreate={createCategory}
-                        // triggerEdit={editCategory}
-                    />
+                    <Modal
+                        title={
+                            <>
+                                <h1 className="mt-4 mb-2 uppercase text-3xl font-semibold text-primary">
+                                    {isEdit ? "Edit" : "Create"} {title}
+                                </h1>
+                            </>
+                        }
+                        open={isFormOpen}
+                        onCancel={() => setIsFormopen(false)}
+                        okText={isEdit ? "Update" : "Create"}
+                        destroyOnHidden
+                        forceRender
+                        footer={null}
+                    >
+                        <Form
+                            form={form}
+                            layout="vertical"
+                            autoComplete="off"
+                            className="mt-5!"
+                        >
+                            <Item
+                                label={
+                                    <label className="label-styling">
+                                        Service Name
+                                    </label>
+                                }
+                                name="service-name"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: "Please input service name!",
+                                    },
+                                ]}
+                            >
+                                <Input
+                                    placeholder="Service name"
+                                    className="input-styling!"
+                                />
+                            </Item>
+                            <Item
+                                label={
+                                    <label className="label-styling">
+                                        Package Name
+                                    </label>
+                                }
+                                name="package-name"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: "Please input Package name!",
+                                    },
+                                ]}
+                            >
+                                <Input
+                                    placeholder="Package name"
+                                    className="input-styling!"
+                                />
+                            </Item>
+                            <Space>
+                                <Item
+                                    label={
+                                        <label className="label-styling">
+                                            Service Price
+                                        </label>
+                                    }
+                                    name="price"
+                                    rules={[
+                                        {
+                                            required: true,
+                                            message:
+                                                "Please input service price!",
+                                        },
+                                    ]}
+                                >
+                                    <Input
+                                        placeholder="Enter the Service price"
+                                        className="input-styling!"
+                                    />
+                                </Item>
+                                <Item
+                                    label={
+                                        <label className="label-styling">
+                                            Time Duration
+                                        </label>
+                                    }
+                                    name="time-duration"
+                                    rules={[
+                                        {
+                                            required: true,
+                                            message: "need time duration!",
+                                        },
+                                    ]}
+                                >
+                                    <Input
+                                        placeholder="Time Duration"
+                                        className="input-styling!"
+                                    />
+                                </Item>
+                            </Space>
+                            <Button className="w-full bg-primary! text-white! hover:border! hover:border-black!">
+                                Create {title}
+                            </Button>
+                        </Form>
+                    </Modal>
                 </Space>
             </Space>
         </>
