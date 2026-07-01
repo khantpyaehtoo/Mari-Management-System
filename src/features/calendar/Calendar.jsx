4aa53f9289d9a2582e7188185e3f64087cc91b30
khantpyaehtoo <ericrebillet@gmail.com>
2026-07-01 17:00:50 +0630

    calendar

--- src/features/calendar/Calendar.jsx
+++ src/features/calendar/Calendar.jsx
@@ -1,4 +1,15 @@
-import { Avatar, Button, Calendar, Flex, Select, Space, Spin } from "antd";
+import {
+    Avatar,
+    Button,
+    Calendar,
+    Col,
+    Flex,
+    Row,
+    Select,
+    Space,
+    Spin,
+    Typography,
+} from "antd";
 import SubHeaderSection from "../../components/SubHeaderSection/SubHeaderSection";
 import { UploadOutlined } from "@ant-design/icons";
 import { useMemo, useRef, useState } from "react";
@@ -107,37 +118,68 @@ const CalendarSection = () => {
                 </Space>
             </Flex>
 
-            <Calendar
-                // cellRender={cellRender}
-                styles={{
-                    root: {
-                        borderRadius: "20px",
-                        marginTop: "20px",
-                    },
-                    header: {
-                        background: "#FBB1BD",
-                        padding: "20px",
-                        borderRadius: "20px 20px 0 0",
-                        boxShadow: "1px 1px 10px rgba(0, 0, 0, 0.2)",
-                    },
-                    body: {
-                        background: "#FFFAF6",
-                        padding: "20px",
-                        border: "2px solid #FBB1BD",
-                        borderRadius: "0 0 20px 20px",
-                        boxShadow: "1px 1px 10px rgba(0, 0, 0, 0.2)",
-                    },
-                    item: {
-                        border: "1px solid #FBB1BD",
-                        borderRadius: "10px",
-                    },
-                }}
-                onSelect={(date, { source }) => {
-                    if (source === "date") {
-                        console.log("Panel Select:", source, date);
-                    }
-                }}
-            />
+            <Row gutter={12}>
+                <Col
+                    lg={6}
+                    className="w-full p-5! rounded-2xl mt-5 broder-2! border-black bg-indigo-600! space-y-5!"
+                >
+                    <Typography.Title level={4} className="text-center!">
+                        Staff Today (June, 30)
+                    </Typography.Title>
+                    <Flex
+                        justify="space-between"
+                        items="center"
+                        className="border-2! border-primary! rounded-2xl! px-10! py-2!"
+                    >
+                        <Space size="middle">
+                            <Avatar
+                                src="https://i.pinimg.com/736x/8a/e9/e9/8ae9e92fa4e69967aa61bf2bda967b7b.jpg"
+                                size={40}
+                            />
+                            <Space size="small" vertical>
+                                <h1 className="text-lg! font-medium!">
+                                    Myo Myo
+                                </h1>
+                                <p className="text-sm!">Nail Artist</p>
+                            </Space>
+                        </Space>
+                        <div className="w-5 h-5 bg-green-500 rounded-full" />
+                    </Flex>
+                </Col>
+                <Col lg={18}>
+                    <Calendar
+                        // cellRender={cellRender}
+                        styles={{
+                            root: {
+                                borderRadius: "20px",
+                                marginTop: "20px",
+                            },
+                            header: {
+                                background: "#FBB1BD",
+                                padding: "20px",
+                                borderRadius: "20px 20px 0 0",
+                                boxShadow: "1px 1px 10px rgba(0, 0, 0, 0.2)",
+                            },
+                            body: {
+                                background: "#FFFAF6",
+                                padding: "20px",
+                                border: "2px solid #FBB1BD",
+                                borderRadius: "0 0 20px 20px",
+                                boxShadow: "1px 1px 10px rgba(0, 0, 0, 0.2)",
+                            },
+                            item: {
+                                border: "1px solid #FBB1BD",
+                                borderRadius: "10px",
+                            },
+                        }}
+                        onSelect={(date, { source }) => {
+                            if (source === "date") {
+                                console.log("Panel Select:", source, date);
+                            }
+                        }}
+                    />
+                </Col>
+            </Row>
         </>
     );
 };
